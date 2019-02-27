de677d5097642d2991c91ca19402e4a3f4e9a10f
Geoff Choy <geoffchoy@outlook.com>
2019-02-27 21:59:40 +1100

    Change to shorter URL

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-example'`, () => {
+  it(`should have as title 'inpowerstrategies'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-example');
+    expect(app.title).toEqual('inpowerstrategies');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-example!');
+    expect(compiled.querySelector('h1').textContent).toContain('Welcome to InPower Strategies!');
   });
 });
